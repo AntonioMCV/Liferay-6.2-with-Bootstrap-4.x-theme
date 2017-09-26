532d08d6d71d243503f7ccfcddb880060a93ed40
VASS\antonio.martinezpere <antonio.martinezperez@vassdigital.com>
2017-09-26 22:45:07 +0200

    add new task and add bootstrap folder

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,16 @@
 
 var gulp = require('gulp');
 var liferayThemeTasks = require('liferay-theme-tasks');
-
+ 
 liferayThemeTasks.registerTasks({
-	gulp: gulp
+	gulp: gulp,
+	hookFn: function(gulp) {
+
+		gulp.hook('before:build:src', function(done) {
+
+			gulp.src('node_modules/bootstrap/*')
+			.pipe(gulp.dest('src/css/bootstrap/'));
+			console.log('bootstrap4 copied');
+		});
+	}
 });
